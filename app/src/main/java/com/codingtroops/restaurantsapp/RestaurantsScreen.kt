b5db1016646683ad9296901950d038cd3fe9d951
Alex Tetervak <tetervak@sheridancollege.ca>
2023-05-22 22:09:31 -0400

    remade the project with newer libs

---
+++ app/src/main/java/com/codingtroops/restaurantsapp/RestaurantsScreen.kt
@@ -0,0 +1,83 @@
+package com.codingtroops.restaurantsapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.codingtroops.restaurantsapp.ui.theme.RestaurantsAppTheme
+
+@Composable
+fun RestaurantsScreen() {
+    LazyColumn(
+        contentPadding = PaddingValues(
+            vertical = 8.dp,
+            horizontal = 8.dp
+        )
+    ) {
+        items(dummyRestaurants) { restaurant ->
+            RestaurantItem(restaurant)
+        }
+    }
+}
+
+@Composable
+fun RestaurantItem(item: Restaurant) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            RestaurantIcon(Icons.Filled.Place, Modifier.weight(0.15f))
+            RestaurantDetails(item.title, item.description, Modifier.weight(0.85f))
+        }
+    }
+}
+
+@Composable
+private fun RestaurantIcon(icon: ImageVector, modifier: Modifier) {
+    Image(
+        imageVector = icon,
+        contentDescription = "Restaurant icon",
+        modifier = modifier.padding(8.dp)
+    )
+}
+
+@Composable
+private fun RestaurantDetails(title: String, description: String, modifier: Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RestaurantsAppTheme {
+        RestaurantsScreen()
+    }
+}
