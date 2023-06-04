d715a07e6d6b31ff120520abdeb7ea50486d2211
Alex Tetervak <tetervak@sheridancollege.ca>
2023-06-04 11:54:42 -0400

    edited the MainViewModel

--- app/src/main/java/com/codingtroops/restaurantsapp/MainViewModel.kt
+++ app/src/main/java/com/codingtroops/restaurantsapp/MainViewModel.kt
@@ -4,13 +4,30 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainViewModel : ViewModel() {
 
+    private var restInterface: RestaurantsApiService
+    private lateinit var restaurantsCall: Call<List<Restaurant>>
+
     private val _uiState: MutableState<List<Restaurant>> =
-        mutableStateOf(dummyRestaurants)
+        mutableStateOf(emptyList())
     val uiState: State<List<Restaurant>> = _uiState
+
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://restaurantsapp-android-default-rtdb.firebaseio.com/")
+            .build()
+        restInterface = retrofit.create(RestaurantsApiService::class.java)
+        getRestaurants()
+    }
 
     fun toggleFavorite(id: Int) {
         val restaurants = uiState.value.toMutableList()
@@ -19,4 +36,29 @@ class MainViewModel : ViewModel() {
         restaurants[itemIndex] = item.copy(isFavorite = !item.isFavorite)
         _uiState.value = restaurants.toList()
     }
+
+    private fun getRestaurants() {
+        restaurantsCall = restInterface.getRestaurants()
+        restaurantsCall.enqueue(
+            object : Callback<List<Restaurant>> {
+                override fun onResponse(
+                    call: Call<List<Restaurant>>,
+                    response: Response<List<Restaurant>>
+                ) {
+                    response.body()?.let { restaurants ->
+                        _uiState.value = restaurants
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Restaurant>>, t: Throwable) {
+                    t.printStackTrace()
+                }
+            })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        restaurantsCall.cancel()
+    }
+
 }
