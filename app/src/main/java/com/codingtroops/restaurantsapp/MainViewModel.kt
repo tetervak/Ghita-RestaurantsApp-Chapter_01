adca20336925c17ac97d090a680837aaf7d69138
Alex Tetervak <tetervak@sheridancollege.ca>
2023-06-04 14:22:38 -0400

    edited MainViewModel

--- app/src/main/java/com/codingtroops/restaurantsapp/MainViewModel.kt
+++ app/src/main/java/com/codingtroops/restaurantsapp/MainViewModel.kt
@@ -4,9 +4,12 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -19,6 +22,10 @@ class MainViewModel : ViewModel() {
     private val _uiState: MutableState<List<Restaurant>> =
         mutableStateOf(emptyList())
     val uiState: State<List<Restaurant>> = _uiState
+
+    private val errorHandler = CoroutineExceptionHandler { _, exception ->
+        exception.printStackTrace()
+    }
 
     init {
         val retrofit: Retrofit = Retrofit.Builder()
@@ -38,22 +45,11 @@ class MainViewModel : ViewModel() {
     }
 
     private fun getRestaurants() {
-        restaurantsCall = restInterface.getRestaurants()
-        restaurantsCall.enqueue(
-            object : Callback<List<Restaurant>> {
-                override fun onResponse(
-                    call: Call<List<Restaurant>>,
-                    response: Response<List<Restaurant>>
-                ) {
-                    response.body()?.let { restaurants ->
-                        _uiState.value = restaurants
-                    }
-                }
-
-                override fun onFailure(call: Call<List<Restaurant>>, t: Throwable) {
-                    t.printStackTrace()
-                }
-            })
+        viewModelScope.launch(errorHandler) {
+            _uiState.value = withContext(Dispatchers.IO){
+                restInterface.getRestaurants()
+            }
+        }
     }
 
     override fun onCleared() {
