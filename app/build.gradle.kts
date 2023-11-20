cf3fe585a4a367636444bddecbef1c90223d4145
Alex Tetervak <tetervak@sheridancollege.ca>
2023-11-20 13:06:53 -0500

    updated libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     defaultConfig {
         applicationId = "ca.tetervak.restaurantapp"
         minSdk = 31
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
