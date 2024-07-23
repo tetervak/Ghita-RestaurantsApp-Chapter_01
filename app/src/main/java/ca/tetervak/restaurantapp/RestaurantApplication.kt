223331fb78f1137337509208f90ba1be0739065d
Alex Tetervak <tetervak@sheridancollege.ca>
2024-07-23 14:05:37 -0400

    refresh on application start

--- app/src/main/java/ca/tetervak/restaurantapp/RestaurantApplication.kt
+++ app/src/main/java/ca/tetervak/restaurantapp/RestaurantApplication.kt
@@ -1,7 +1,23 @@
 package ca.tetervak.restaurantapp
 
 import android.app.Application
+import ca.tetervak.restaurantapp.data.repository.RestaurantRepository
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
-class RestaurantApplication() : Application()
+class RestaurantApplication() : Application(){
+
+    @Inject
+    lateinit var repository: RestaurantRepository
+
+    override fun onCreate() {
+        super.onCreate()
+
+        MainScope().launch {
+            repository.refreshRestaurants()
+        }
+    }
+}
